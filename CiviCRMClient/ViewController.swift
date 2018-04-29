868e2b71c09fe902d9b35165baec8e008512c462
rtvta <roman.tiagni@gmail.com>
2018-04-29 07:58:49 +0300

    Initial Commit

---
+++ CiviCRMClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CiviCRMClient
+//
+//  Created by Roman Tiagni on 29/04/2018.
+//  Copyright © 2018 Roman Tiagni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
